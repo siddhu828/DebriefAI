dc47631b281c389201bcc5f07f949d590c53ae0f
siddhu828 <145180939+siddhu828@users.noreply.github.com>
2025-07-19 23:17:29 +0530

    more  changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useEffect } from "react-router-dom";
 import MainLayout from './layout/MainLayout'
 import Upload from "./pages/Upload";
 import Record from "./pages/Record";
